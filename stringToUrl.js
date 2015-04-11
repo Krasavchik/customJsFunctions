cf77de317d84974b7f32c76105bc8a389e80b411
Krasavchik <ml.videaux@gmail.com>
2015-04-11 21:58:42 +0200

    update stringToUrl function

--- stringToUrl.js
+++ stringToUrl.js
@@ -8,12 +8,18 @@ function stringToUrl(mystring){
         ["ú|ù|ü","u"],
         ["ñ","n"]
     ];
+    mystring = mystring.toLowerCase();
+
     for(var i = 0, x = specialChars.length; i < x; i++){
-        var re = new RegExp(specialChars[i][0],"gi");
+        var re = new RegExp(specialChars[i][0],"g");
         mystring = mystring.replace(re,specialChars[i][1]);
     }
+
+    mystring = mystring.replace(/-{2,}/g, "-");
+
     while(mystring.slice(-1) == "-"){
         mystring = mystring.substring(0, mystring.length - 1);
     }
+
     return mystring;
-};
+}
