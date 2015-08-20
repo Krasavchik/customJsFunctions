2b9579251f4a6d0a25934ed7ddf91f7661fe96da
Krasavchik <matthieu.lavergne@gmail.com>
2015-08-20 13:35:29 +0200

    update geocoder script

--- geoCoder.js
+++ geoCoder.js
@@ -1,21 +1,14 @@
-function geoNorm(address){
-  var geocoder = Maps.newGeocoder();
-  var location = geocoder.geocode(address);
-
-  if (location.status == 'OK') {
-
-    var geoNorm = [];
-    geoNorm.push("geoCodeX:"+location["results"][0]["geometry"]["location"]["lng"],"geoCodeY:"+location["results"][0]["geometry"]["location"]["lat"]);
-
-    var component = location["results"][0].address_components;
-
-    var i = location["results"][0].address_components.length - 1;
-
-    while(i >= 0){
-      geoNorm.push(component[i].types+":"+component[i].long_name);
-      i--;
-    }
-
+function reverseGeoNorm(geoX, geoY){
+  var geocoder = Maps.newGeocoder().setLanguage('fr');
+  var location = geocoder.reverseGeocode(geoX, geoY);
+    if (location.status == 'OK') {
+      var geoNorm = [];
+      var component = location["results"][0].address_components;
+      var i = location["results"][0].address_components.length - 1;
+      while(i >= 0){
+        geoNorm.push(component[i].types+":"+component[i].long_name);
+        i--;
+      }
     return geoNorm.join("/");
   }
 }
